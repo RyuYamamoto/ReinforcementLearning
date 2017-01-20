faa180d70f2fefe738688faa32903ec4a75e5764
Ryu Yamamoto <ron_robotics@yahoo.co.jp>
2017-01-20 11:50:55 +0900

    display board

---
+++ TTT_board/TTT_board.py
@@ -0,0 +1,29 @@
+PLAYER1 = 'Human'
+PLAYER2 = 'Human'
+
+class TTT_board:
+    def __init__(self, player1='Human', player2='AI'):
+        self.field      = []
+        self.win        = None
+        self.lose       = None
+        self.player1    = player1
+        self.player2    = player2
+
+        print ('[' + self.player1 + ' vs ' + self.player2 + ']\n')
+
+        for i in range(9):self.field.append('-')
+
+    def print_field(self):
+        row = ' {} | {} | {} '
+        hr = '\n-----------\n'
+
+        print((row + hr + row + hr + row).format(*self.field))
+
+class TTT_Agent:
+    def __init__(self, mode='RANDOM'):
+        self.mode = mode
+
+if __name__ == '__main__':
+    ttt = TTT_board(PLAYER1, PLAYER2)
+
+    ttt.print_field()
