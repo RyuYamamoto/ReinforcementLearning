ddead98d74847cab437bf28d692569ec8cb11b85
RyuYamamoto <ron_robotics@yahoo.co.jp>
2017-11-05 17:09:51 +0900

    temp commit

--- gym/Cartpole.py
+++ gym/Cartpole.py
@@ -1,4 +1,33 @@
 import gym
+import numpy as np
+
+division = 6
+max_step = 200
+
+class QLearning:
+    def __init__(self, env, ratio, alpha):
+        self.env        = env
+        self.ratio      = ratio
+        self.alpha      = alpha
+        self.qtable     = np.zeros(division**4, self.env.action_space.n)
+
+    def get_action(self):
+
+    def update_Qtable(self):
+
+    def run(self, max_espisode):
+        for i_episode in range(max_episode):
+            observation = self.env.reset()
+            for t in range(max_step):
+                if is_done is True:
+                    self.env.render()
+                    print
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v0')
+    qlearning = QLearning(env, 0.5, 0.5)
+    qlearning.run(1000)
+
 env = gym.make('CartPole-v0')
 for i_episode in range(20):
     observation = env.reset()
