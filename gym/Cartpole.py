227de5a8c70395e33f00a1325e6cef33a0fd7c35
RyuYamamoto <rottertarmenation.y11.r18@gmail.com>
2017-11-08 18:38:56 +0900

    temp commit

--- gym/Cartpole.py
+++ gym/Cartpole.py
@@ -11,17 +11,30 @@ class QLearning:
         self.alpha      = alpha
         self.qtable     = np.zeros(division**4, self.env.action_space.n)
 
+		'''
+			状態の離散化
+		'''
+		def state_discretization(self):
+
+		'''
+			t+1時刻での行動
+		'''
     def get_action(self):
 
+		'''
+			Q値の更新
+		'''
     def update_Qtable(self):
 
+		'''
+			メインループ
+		'''
     def run(self, max_espisode):
         for i_episode in range(max_episode):
             observation = self.env.reset()
             for t in range(max_step):
                 if is_done is True:
                     self.env.render()
-                    print
 
 if __name__ == '__main__':
     env = gym.make('CartPole-v0')
